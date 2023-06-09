fa42425eb1e9ce78edb6b7acac3c7e7fb63aa34f
noornammou <96783014+noornammou@users.noreply.github.com>
2023-06-09 08:26:51 +0200

    7

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -29,7 +29,7 @@ class SignUpView(APIView):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
     
-    def put(self, request):
+    def post(self, request):
         email = request.data.get('email')
         first_name = request.data.get('first_name')
         last_name = request.data.get('last_name')
